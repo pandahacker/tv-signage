09303646e7615cf397d041d5944706a27397a715
pandahacker <pandahacker@pandahacker.com>
2022-05-25 11:18:09 -0400

    fix ipv6 prefix

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ app.use(
 );
 
 app.get("/upload", (req, res) => {
-  console.log(req.ip);
+  //console.log(req.ip);
   ip = req.ip
+  ip = ip.slice(0, 5);
   if(ip === process.env.HOME_ || ip === process.env.WORK) {
     res.sendFile(path.join(__dirname, "upload.html"));
   }else{
